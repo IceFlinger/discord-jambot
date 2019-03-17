6474b443899637d36029df3c90b1217089077914
IceFlinger <ice@loud.house>
2019-03-17 16:03:23 -0400

    flags

--- modules/flags.py
+++ modules/flags.py
@@ -8,119 +8,98 @@ import hashlib
 class moduleClass(botmodule):
 
 	def default_config(self):
-		return {"downloads": [{"command": "example", 
-		"cache": True,
-		 "web_folder": "https://example.com/images/"}],
-		"uploads": [{"local_folder": "/srv/www/images",
-		"command": "example", 
+		return {"downloads": {"example":{ "cache": True, "web_folder": "https://example.com/images/"}},
+		"uploads": {"example":
+		{"local_folder": "/srv/www/images",
 		"raw_folder": "/backup/unconverted",
 		"max_filesize": 4000000,
 		"resize": False,
 		"resize_w": 500,
 		"resize_h": 192,
-		"resize_tolerence": 0.8}]
+		"resize_tolerence": 0.8}}
 		}
 
 	async def on_ready(self, client, config):
 		self.cached = {}
 		self.cached_list = {}
 		for download in config["downloads"]:	
-			self.cached[download["command"]] = False
-			self.cached_list[download["command"]] = []
+			self.cached[download] = False
+			self.cached_list[download] = []
 
-	def upload_flag(self, client, config, message):
+	async def upload_flag(self, client, config, message):
 		cmd = await client.get_cmd(message)
 		key = cmd["cmd"]
 		self.cached[key] = False
 		try:
 			urls = []
-			if len(message.embeds) > 0:
-				for link in message.embeds:
-					urls.append(link.url)
-			
-			r = requests.get(url, stream=True)
-			size = 0
-			ctt = io.BytesIO()
-			for chunk in r.iter_content(2048):
-				size += len(chunk)
-				ctt.write(chunk)
-				if size > self.get("max_filesize"):
-					r.close()
-					self.send(e.target, "That file is too big")
-					raise
-			content = ctt.getvalue()
-			img_in = Image.open(io.BytesIO(content))
-			hash = hashlib.md5()
-			hash.update(content)
-			i = hash.hexdigest()
-			if img_in.format == "GIF" and not self.get("resize"):
-				img_in.save(self.get("local_folder") + i + '.gif', 'GIF', save_all=True, optimize=False)
-				self.send(e.target, self.get("web_folder") + i + ".gif")
-			else:
-				img_size = img_in.size
-				if img_size[0] < img_size[1] and self.get("orent_check") == "long":
-					self.output("That image isn't long", ("", source, target, c, e))
-					raise
-				elif img_size[0] > img_size[1] and self.get("orent_check") == "tall":
-					self.output("That image isn't tall", ("", source, target, c, e))
-					raise
-				if self.get("save_unconverted"):
-					img_in.save(self.get("unconverted_folder") + i + '.png', 'PNG')
-				img_out = img_in
-				if self.get("resize"):
-					x = self.get("resize_width")
-					y = self.get("resize_height")
-					if self.get("border"):
-						border = self.get("border_size")
-						img_out = img_in.resize((x-(border*2),y-(border*2)), Image.ANTIALIAS)
-						img_out = ImageOps.expand(img_out,border=border,fill='black')
-					else:
+			if len(message.attachments) > 0:
+				for link in message.attachments:
+					urls.append((link.url, link.filename))
+			#append args
+			print(urls)
+			for urlp in urls:
+				filename = urlp[1]
+				url = urlp[0]
+				r = requests.get(url, stream=True)
+				size = 0
+				ctt = io.BytesIO()
+				for chunk in r.iter_content(2048):
+					size += len(chunk)
+					ctt.write(chunk)
+					if size > config["uploads"][key]["max_filesize"]:
+						r.close()
+						await message.channel.send("That file is too big")
+						raise
+				content = ctt.getvalue()
+				img_in = Image.open(io.BytesIO(content))
+				hash = hashlib.md5()
+				hash.update(content)
+				i = hash.hexdigest()
+				if img_in.format == "GIF" and not config["uploads"][key]["resize"]:
+					img_in.save(config["uploads"][key]["local_folder"] + i + '.gif', 'GIF', save_all=True, optimize=False)
+					if key in config["downloads"]:
+						await message.channel.send(config["uploads"][key]["web_folder"] + i + ".gif")
+				else:
+					if config["uploads"][key]["save_unconverted"]:
+						img_in.save(config["uploads"][key]["unconverted_folder"] + filename + '-' + i + '.png', 'PNG')
+					img_out = img_in
+					if config["uploads"][key]["resize"]:
+						x = config["uploads"][key]["resize_w"]
+						y = config["uploads"][key]["resize_h"]
 						img_out = img_in.resize((x,y), Image.ANTIALIAS)
-				img_out.save(self.get("local_folder") + i + '.png', 'PNG')
-				self.send(e.target, self.get("web_folder") + i + ".png")
-			if self.get("flag_logfile") != "":
-				dump = open(self.get("flag_logfile"), "a")
-				dump.write(str(time.time()) + "\t" + e.source.nick + "\t" + i + "\n")
-				dump.close()
-			return True
+					img_out.save(config["uploads"][key]["local_folder"] + i + '.png', 'PNG')
+					if key in config["downloads"]:
+						await message.channel.send(config["downloads"][key]["web_folder"] + i + ".png")
+			return
 		except:
-			self.send(e.target, "Problem flagging that")
-			if debug:
-				raise
-			else:
-				pass
-		return False
+			await message.channel.send("Problem flagging that")
+			pass
+		return
 
 	async def on_message(self, client, config, message):
+		print(message)
 		cmd = await client.get_cmd(message)
 		uploaded = False
 		if cmd:
+			#redo logic here/check args for upload case
 			if config["uploads"] != None:
 				for upload in config["uploads"]:
-					if cmd["cmd"] == upload["command"]:
+					if cmd["cmd"] == upload:
 						async with message.channel.typing():
-							uploaded = self.upload_flag(client, config, message)
-							if uploaded:
-								url = False
-								for download in config["downloads"]:
-									if upload["command"] == download["command"]:
-										url = download["web_folder"] + uploaded
-								if url:
-									await message.channel.send(url)
-								else:
-									await message.channel.send("Uploaded")
+							await self.upload_flag(client, config, message)
+							uploaded = True
 			if not uploaded:
 				for download in config["downloads"]:
-					if cmd["cmd"] == download["command"]:
+					if cmd["cmd"] == download:
 						async with message.channel.typing():
-							if (not self.cached[download["command"]]) or (not download["cache"]):
-								r = requests.get(download["web_folder"])
+							if (not self.cached[download]) or (not config["downloads"][download]["cache"]):
+								r = requests.get(config["downloads"][download]["web_folder"])
 								d = html.fromstring(r.content)
 								imglist = d.xpath('//a[@href]/@href')
 								imglist = imglist[5:] #first 4 links are random index shit, lazy way to skip (add check for image extension)
-								self.cached_list[download["command"]] = imglist
-								self.cached[download["command"]] = True
+								self.cached_list[download] = imglist
+								self.cached[download] = True
 							else:
-								imglist  = self.cached_list[download["command"]] 
+								imglist  = self.cached_list[download] 
 							imgid = random.randint(0,len(imglist)-1)
-						await message.channel.send(download["web_folder"] + imglist[imgid])
+						await message.channel.send(config["downloads"][download]["web_folder"] + imglist[imgid])
